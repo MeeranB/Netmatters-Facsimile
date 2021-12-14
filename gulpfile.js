abd7c880412be4f0bb7b9ad181484ecc0d6f7a4c
Meeran <v9@live.co.uk>
2021-12-14 09:17:04 +0000

    Add compile and minify process via gulp & Switch to CDN for font-awesome

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,26 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
+"use strict";
+
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const uglifycss = require("gulp-uglifycss");
+const sourcemaps = require("gulp-sourcemaps");
+
+function compileSass() {
+    return gulp
+        .src("./app/scss/**/*.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass().on("error", sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("./app/assets/css"));
 }
 
-exports.default = defaultTask;
+function minifyCSS() {
+    return gulp
+        .src("./app/assets/css/*.css")
+        .pipe(uglifycss({ uglyComments: true }))
+        .pipe(gulp.dest("./dist/"));
+}
+
+exports.default = function () {
+    gulp.watch("./app/scss/**/*.scss", gulp.series(compileSass, minifyCSS));
+};
